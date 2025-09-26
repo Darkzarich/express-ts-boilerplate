ad77c269fbeac943fd59923cbecb370bcd210ba6
Darkzarich <darkzarichv2@gmail.com>
2025-09-26 22:36:06 +0300

    update most deps to their latest versions - typescript@5.8.3, express@5.1.0 etc

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-import bodyParser from 'body-parser';
 
 const app = express();
 
 app.use(cors());
 
-app.use(bodyParser.json());
-
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+// Parse JSON and URL-encoded data
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json({}));
 
 app.get('/', (req, res) => {
   res.send('Express + TypeScript Server');
